8ac4970e0eb1d332319217b26b230be2ca09f004
sanhapon <sanhapon@yahoo.com>
2022-09-01 14:49:55 +0700

    call api

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,36 +1,35 @@
 use hyper::Client;
-use hyper::Error;
-use hyper::client::ResponseFuture;
-use hyper::body::HttpBody as _;
-use tokio::io::{stdout, AsyncWriteExt as _};
-use std::thread;
+use http::uri::Uri;
+use std::io;
+use std::sync::{Arc};
+use tokio::sync::Semaphore;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+
     println!("start");
+    let semaphore = Arc::new(Semaphore::new(100));
 
-    let client = Client::new();
+    for i in 1 .. 1000000 {
+        let permit = semaphore.clone().acquire_owned().await.unwrap();
 
-    // for i in 1 .. 100 {
-    //     let uri = "http://localhost:3030/properties".parse()?;
-    //     let mut resp = client.get(uri).await?;
+        tokio::spawn(async move {
+            let client = Client::new();
+            let resp = client.get(Uri::from_static("http://127.0.0.1:3030/properties")).await;
+            match resp {
+                Ok(r) => {
+                    println!("{i}: {}", r.status());
+                    drop(permit);
+                },
+                Err(err) => {
+                    println!("error --> {err}");
+                    drop(permit);
+                }
+            }
+        });
+    }
 
-    //     println!("{i} Response: {}", resp.status());
-    // }
-
-    let handler = thread::spawn(async || {
-
-        println!("shooting");
-        let mut resp = client.get(uri);
-
-        let mut resp = client.get(uri).await?;
-
-        println!("hello")
-
-        1
-    });
-
-    println!("done");
-
+    let _ = io::stdin().lines();
+    println!("end");
     Ok(())
-}์
+}
