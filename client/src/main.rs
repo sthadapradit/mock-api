3a28ec74b87fd649d00770d32c88f56a07ea1f14
sanhapon <sanhapon@yahoo.com>
2022-09-02 19:51:53 +0700

    read env

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,36 +1,55 @@
+use http::Uri;
 use hyper::Client;
-use http::uri::Uri;
 use std::sync::{Arc};
 use tokio::sync::Semaphore;
 use stopwatch::{Stopwatch};
 use std::io;
+use std::env;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
 
     println!("start");
-    let semaphore = Arc::new(Semaphore::new(50));
+    let env_url: String = env::var("url").expect("$url is not set");    
+    let env_concurrent: String = env::var("concurrent").expect("$parallel is not set");    
+    println!("send req to {env_url}");
+
+    let url: Uri = env_url.parse()?;
+
+    let semaphore = Arc::new(Semaphore::new(env_concurrent.parse::<usize>().unwrap()));
+
     let sw = Stopwatch::start_new();
+    let client = Client::new();
 
     for i in 1 .. 100000000 {
         let permit = semaphore.clone().acquire_owned().await.unwrap();
 
+        let tmp_client = client.clone();
+        let tmp_url = url.clone();
+
+        if i % 100 == 0 {
+            println!("\nsend {i}");
+        }
+
+        
         tokio::spawn(async move {
-            let client = Client::new();
-            let resp = client.get(Uri::from_static("http://127.0.0.1:3030/properties")).await;
+            let resp = tmp_client.get(tmp_url).await;
+
             match resp {
-                Ok(r) => {
-                    match hyper::body::to_bytes(r.into_body()).await {
-                        Ok(x)=>{
-                            println!("{i}");
-                            // let y = x.to_ascii_lowercase();
-                            // print!("{:?}", y[0]);
-                        },
-                        Err(err) => {
-                            println!("err: {err}");
-                        }
-                    }
+                Ok(_r) => {
+                    print!(".");
                     drop(permit);
+                    
+                    //     Ok(x)=>{
+                    //         // println!("{i}");
+                    //         // let y = x.to_ascii_lowercase();
+                    //         // print!("{:?}", y[0]);
+                    //     },
+                    //     Err(err) => {
+                    //         println!("err: {err}");
+                    //     }
+                    // }
+                    // drop(permit);
                 },
                 Err(err) => {
                     println!("error --> {err}");
